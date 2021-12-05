47d9f0812daeb98b808ffd6bc5928d739e7e3c18
Hasnaa Al-Habahbeh <hasnaahab98@gmail.com>
2021-12-05 17:03:49 +0200

    finished the lab

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.scss';
+import Header from './components/header/index';
+import Footer from './components/footer/index';
+import Form from './components/form/index';
+import Results from './components/results/index';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: null,
+      requestParams: {},
+    };
+  }
+
+  callApi = (requestParams) => {
+    // mock output
+    const data = {
+      count: 2,
+      results: [
+        {name: 'fake thing 1', url: 'http://fakethings.com/1'},
+        {name: 'fake thing 2', url: 'http://fakethings.com/2'},
+      ],
+    };
+    this.setState({data, requestParams});
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <Header />
+        <div className='textDev'>Request Method: {this.state.requestParams.method}</div>
+        <div className='textDev'>URL: {this.state.requestParams.url}</div>
+        <Form handleApiCall={this.callApi} />
+        <Results data={this.state.data} />
+        <Footer />
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/footer/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './footer.scss';
+
+export default function Footer(){
+  return(
+    <footer>&copy; 2021</footer>
+  )
+}
